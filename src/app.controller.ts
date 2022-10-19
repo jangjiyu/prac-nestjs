2f4494c848eb0d9cef4d095ba15fca207b457505
Jiyu Jang <apryll7995@gmail.com>
2022-10-19 22:57:20 +0900

    fitst commit

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('api')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('user') // [GET] /api/user
+  getUser(): string {
+    return this.appService.getUser();
+  }
+
+  @Post('user') // [POST] /api/user
+  postUser(): string {
+    return this.appService.postUser();
+  }
+}
