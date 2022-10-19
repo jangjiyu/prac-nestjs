41bd9bb6e98ffb272200ecf18d713d12671d0b7c
Jiyu Jang <apryll7995@gmail.com>
2022-10-20 01:21:32 +0900

    example of importing environment variables from an external server

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,14 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// 외부 서버(aws 비밀저장소, 구글클라우드 플랫폼 비밀관리자 등)에서 환경변수 불러올 때 + ConfigModule 옵션에 load: [getEnv(이건 함수명))] 추가
+const getEnv = async () => {
+  const res = axios.get('/secretKey');
+  return res.data;
+};
+
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [getEnv] })],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
