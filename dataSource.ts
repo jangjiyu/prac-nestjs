ae17620c48759ad5c2f1fbcb1adc80f8705d0aa8
Jiyu Jang <apryll7995@gmail.com>
2022-11-04 18:54:39 +0900

    add typeorm setting(connecting to mysql) and create entities

---
+++ dataSource.ts
@@ -0,0 +1,35 @@
+import { DataSource } from 'typeorm';
+import { ChannelChats } from './src/entities/ChannelChats';
+import { ChannelMembers } from './src/entities/ChannelMembers';
+import { Channels } from './src/entities/Channels';
+import { DMs } from './src/entities/DMs';
+import { Mentions } from './src/entities/Mentions';
+import { Users } from './src/entities/Users';
+import { WorkspaceMembers } from './src/entities/WorkspaceMembers';
+import { Workspaces } from './src/entities/Workspaces';
+import 'dotenv/config';
+
+const dataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [
+    ChannelChats,
+    ChannelMembers,
+    Channels,
+    DMs,
+    Mentions,
+    Users,
+    WorkspaceMembers,
+    Workspaces,
+  ],
+  migrations: [__dirname + '/src/migrations/*.ts'],
+  charset: 'utf8mb4_general_ci',
+  synchronize: false,
+  logging: true,
+});
+
+export default dataSource;
--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,45 @@ import { UsersModule } from './users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { ChannelsModule } from './channels/channels.module';
 import { DmsModule } from './dms/dms.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChannelChats } from './entities/ChannelChats';
+import { ChannelMembers } from './entities/ChannelMembers';
+import { Channels } from './entities/Channels';
+import { DMs } from './entities/DMs';
+import { Mentions } from './entities/Mentions';
+import { Users } from './entities/Users';
+import { WorkspaceMembers } from './entities/WorkspaceMembers';
+import { Workspaces } from './entities/Workspaces';
+import 'dotenv/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      // entities: ['entities/*.ts'],
+      // autoLoadEntities: true, // entities 자동 생성
+      entities: [
+        ChannelChats,
+        ChannelMembers,
+        Channels,
+        DMs,
+        Mentions,
+        Users,
+        WorkspaceMembers,
+        Workspaces,
+      ],
+      synchronize: false,
+      logging: true,
+      migrations: [__dirname + '/migrations/*.ts'],
+      keepConnectionAlive: true, // hot reload 될 때 db 연결 끊기는 것 방지
+      charset: 'utf8mb4',
+    }),
     UsersModule,
     WorkspacesModule,
     ChannelsModule,
